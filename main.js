b5f186d59afddc06ffcd6a733c7d1250d4664db5
alepmont <ale26gonz@gmail.com>
2023-06-10 02:25:32 -0300

    mi proyecto en github

---
+++ main.js
@@ -0,0 +1,116 @@
+// PRODUCTOS
+const  productos  =  [
+    // Abrigos
+    {
+        id : "campera-01" ,
+        titulo : "Campera 01" ,
+        imagen : "./img/campera/01" ,
+        categoría : {
+            nombre : "Campera" ,
+            id : "campera"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "campera-02" ,
+        titulo : "Campera 02" ,
+        imagen : "./img/campera/02.jpg" ,
+        categoría : {
+            nombre : "Campera" ,
+            id : "campera"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "campera-03" ,
+        titulo : "Campera 03" ,
+        imagen : "./img/campera/03.jpg" ,
+        categoría : {
+            nombre : "Campera" ,
+            id : "campera"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "campera-04" ,
+        titulo : "Campera 04" ,
+        imagen : "./img/campera/04.jpg" ,
+        categoría : {
+            nombre : "Campera" ,
+            id : "campera"
+        } ,
+        precio : 1000
+    } ,
+    // Camisas
+    {
+        id : "camisa-01" ,
+        titulo : "Camisa 01" ,
+        imagen : "./img/camisa/01.jpg" ,
+        categoría : {
+            nombre : "Camisa" ,
+            id : "camisa"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "camisa-02" ,
+        titulo : "Camisa 02" ,
+        imagen : "./img/camisa/02.jpg" ,
+        categoría : {
+            nombre : "Camisa" ,
+            id : "camisa"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "camisa-03" ,
+        titulo : "Camisa 03" ,
+        imagen : "./img/camisa/03.jpg" ,
+        categoría : {
+            nombre : "Camisa" ,
+            id : "camisa"
+        } ,
+        precio : 1000
+    } ,
+    // Gorros
+    {
+        id : "gorro-01" ,
+        titulo : "Gorro 01" ,
+        imagen : "./img/gorro/01.jpg" ,
+        categoría : {
+            nombre : "Gorro" ,
+            id : "gorro"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "gorro-02" ,
+        titulo : "Gorro 02" ,
+        imagen : "./img/gorro/02.jpg" ,
+        categoría : {
+            nombre : "Gorro" ,
+            id : "gorro"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "gorro-03" ,
+        titulo : "Gorro 03" ,
+        imagen : "./img/gorro/03.jpg" ,
+        categoría : {
+            nombre : "Gorro" ,
+            id : "gorro"
+        } ,
+        precio : 1000
+    } ,
+    {
+        id : "gorro-04" ,
+        titulo : "Gorro 04" ,
+        imagen : "./img/gorro/04.jpg" ,
+        categoría : {
+            nombre : "Gorro" ,
+            id : "gorro"
+        } ,
+        precio : 1000
+    } 
+] ;
